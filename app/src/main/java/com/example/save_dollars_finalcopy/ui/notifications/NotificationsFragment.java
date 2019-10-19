4ccde34f92bb05e727cbf5670b8f7b0f7b55e56d
SaveDollarsApp <56237362+SaveDollarsApp@users.noreply.github.com>
2019-10-19 16:02:01 -0700

    Worked On with Zander

--- app/src/main/java/com/example/save_dollars_finalcopy/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/save_dollars_finalcopy/ui/notifications/NotificationsFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,16 +21,28 @@ public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater,
+                             final ViewGroup container, final Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        final ScrollView sv = root.findViewById(R.id.scrollView2);
+        final TextView input = root.findViewById(R.id.notInput);
+        final LinearLayout lilay = (LinearLayout) root.findViewById(R.id.linearLayout);
+        Button add = root.findViewById(R.id.notButton);
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                TextView tx = new TextView(input.getContext());
+                lilay.addView(tx);
+//                sv.addView(v);
             }
         });
         return root;
